3a131df73a5d6b6f01b012b19e67ac06b062b0ba
Gar1695 <139810535+Gar1695@users.noreply.github.com>
2023-07-17 15:56:12 -0500

    Update atacante.py

--- atacante.py
+++ atacante.py
@@ -2,7 +2,7 @@ from socket import socket
 
 
 # Definimos la dirección y puerto del servidor (Siempre de la máquina víctima)
-server_address = ('192.168.6.38', 5000)
+server_address = ('tu_direccion_ip', 5000)
 
 # Creamos el socket cliente, ya que restablecemos la conexión a cada comando que se ejecute
 client_socket = socket()

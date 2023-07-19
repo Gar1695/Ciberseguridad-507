072730e0e5f6bb3aad0791113787a26b6cea900b
Gar1695 <139810535+Gar1695@users.noreply.github.com>
2023-07-19 16:54:31 -0500

    Update cliente.sh

--- cliente.sh
+++ cliente.sh
@@ -1,51 +1,41 @@
 #!/bin/bash
 
-# Función para leer datos del servidor y mostrarlos en la máquina cliente
-function ReadData_TCP {
-    param($Stream)
-    $Buffer = New-Object Byte[] 1024
-    $Encoding = New-Object System.Text.AsciiEncoding
-
-    $Data = ""
-    while ($Stream.DataAvailable) {
-        $ReadBytes = $Stream.Read($Buffer, 0, $Buffer.Length)
-        $Data += $Encoding.GetString($Buffer, 0, $ReadBytes)
-    }
-
-    Write-Output $Data
-}
-
 # Dirección IP y Puerto del servidor
-$serverIP = "IP_DEL_SERVIDOR"
-$serverPort = 443
+serverIP="IP_DEL_SERVIDOR"
+serverPort=443
 
 # Establecer conexión con el servidor
-$client = New-Object System.Net.Sockets.TcpClient
-$client.Connect($serverIP, $serverPort)
-$stream = $client.GetStream()
+exec 3<>/dev/tcp/$serverIP/$serverPort
 
-# Bucle para leer comandos del usuario y enviarlos al servidor
-while ($true) {
-    # Leer el comando ingresado por el usuario
-    $command = Read-Host "Ingrese un comando (o 'exit' para salir)"
-
-    # Salir si el comando es 'exit'
-    if ($command -eq "exit") {
-        break
-    }
-
-    # Enviar el comando al servidor
-    $writer = New-Object System.IO.StreamWriter $stream
-    $writer.WriteLine($command)
-    $writer.Flush()
-
-    # Leer la salida del comando ejecutado en el servidor y mostrarla en la máquina cliente
-    $output = ReadData_TCP $stream
-    Write-Output $output
+# Función para leer la respuesta del servidor y mostrarla en la máquina cliente
+function ReadResponse {
+    local response
+    while IFS= read -r -t 0.1 -u 3 line; do
+        response+="$line"$'\n'
+    done
+    echo "$response"
 }
 
-# Cerrar la conexión y el cliente
-$stream.Close()
-$client.Close()
+# Bucle para leer comandos del usuario y enviarlos al servidor
+while true; do
+    # Leer el comando ingresado por el usuario
+    read -p "Ingrese un comando (o 'exit' para salir): " command
+
+    # Salir si el comando es 'exit'
+    if [ "$command" = "exit" ]; then
+        break
+    fi
+
+    # Enviar el comando al servidor
+    echo "$command" >&3
+
+    # Leer la salida del comando ejecutado en el servidor y mostrarla en la máquina cliente
+    response=$(ReadResponse)
+    echo "$response"
+done
+
+# Cerrar la conexión
+exec 3<&-
+exec 3>&-
 
 

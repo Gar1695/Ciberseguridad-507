e206a7dc20800ed221cc7911763d0801ec2489d6
Gar1695 <139810535+Gar1695@users.noreply.github.com>
2023-07-19 17:15:55 -0500

    Update cliente.sh

--- cliente.sh
+++ cliente.sh
@@ -7,15 +7,6 @@ serverPort=443
 # Establecer conexión con el servidor
 exec 3<>/dev/tcp/$serverIP/$serverPort
 
-# Función para leer la respuesta del servidor y mostrarla en la máquina cliente
-function ReadResponse {
-    local response
-    while IFS= read -r -t 0.1 -u 3 line; do
-        response+="$line"$'\n'
-    done
-    echo "$response"
-}
-
 # Bucle para leer comandos del usuario y enviarlos al servidor
 while true; do
     # Leer el comando ingresado por el usuario
@@ -23,19 +14,19 @@ while true; do
 
     # Salir si el comando es 'exit'
     if [ "$command" = "exit" ]; then
+        echo "exit" >&3
         break
     fi
 
     # Enviar el comando al servidor
     echo "$command" >&3
 
-    # Leer la salida del comando ejecutado en el servidor y mostrarla en la máquina cliente
-    response=$(ReadResponse)
-    echo "$response"
+    # Leer la respuesta del servidor y mostrarla en la máquina cliente
+    while IFS= read -r -t 0.1 -u 3 line; do
+        echo "$line"
+    done
 done
 
 # Cerrar la conexión
 exec 3<&-
 exec 3>&-
-
-
